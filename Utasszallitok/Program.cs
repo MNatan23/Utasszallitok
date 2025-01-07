cfe38543fd0e40430c9d73f77a5ddbb1c79b0192
MNatan23 <163414251+MNatan23@users.noreply.github.com>
2025-01-07 13:06:20 +0100

    Add project files.

---
+++ Utasszallitok/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Utasszallitok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Repulo> Repulok = new();
+            foreach (var sor in File.ReadAllLines("Utasszallitok.txt").Skip(1))
+            {
+                Repulok.Add(new Repulo(sor));
+                Console.WriteLine($"4.feladat \t Adatsorok száma: {Repulok.Count()}");
+
+                int db = 0;
+                foreach (var b in Repulok)
+                {
+                    if (b.Típus == "")
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Utasszallitok/Repulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utasszallitok
+{
+    internal class Repulo
+    {
+        public string Típus { get; set; }
+        public int Év { get; set; }
+        public string Utas { get; set; }
+        public string Szemelyzet { get; set; }
+        public int UtazoSebbeseg { get; set; }
+        public int Felszállótömeg { get; set; }
+        public string Fesztáv { get; set; }
+
+        public Repulo(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Típus = s[0];
+            Év =int.Parse( s[1]);
+            Utas = s[2];
+            Szemelyzet = s[3];
+            UtazoSebbeseg = int.Parse(s[4]);
+            Felszállótömeg = int.Parse(s[5]);
+            Fesztáv = s[6];
+
+        }
+    }
+
+}
